889c91870547d72a6f3de963221c23ecbb04c6a4
caseywatts <casey.s.watts@gmail.com>
2016-01-17 18:18:41 -0500

    tocify do not extend the page (pages get absurdly long)

--- javascript/custom.js
+++ javascript/custom.js
@@ -3,6 +3,7 @@ $(function() {
   //Calls the tocify method on your HTML div.
   $("#toc").tocify({
     selectors: "h2,h3,h4",
+    extendPage: false,
     showEffect: "fadeIn"
   });
   if( $('#toc').children('ul').length < 3){
