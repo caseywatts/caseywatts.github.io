27304ed9cce7e39e64faf9ad69c81b4406781aac
caseywatts <casey.s.watts@gmail.com>
2016-01-17 17:18:01 -0500

    install jquery-tocify

---
+++ javascript/custom.js
@@ -0,0 +1,8 @@
+//Executes your code when the DOM is ready.  Acts the same as $(document).ready().
+$(function() {
+//Calls the tocify method on your HTML div.
+$("#toc").tocify({selectors: "h2,h3,h4", showEffect: "fadeIn"});
+if( $('#toc').children('ul').length < 3){
+ $("#toc").hide();
+}
+});
