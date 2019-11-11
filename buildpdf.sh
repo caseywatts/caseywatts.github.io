12a59bf51a0ae80c3c11913f49f707a54636101a
Casey Watts <casey.s.watts@gmail.com>
2019-11-11 16:15:57 -0500

    add book publishing files

---
+++ buildpdf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+pandoc -o debuggingyourbrain.pdf -f markdown_github+yaml_metadata_block -t latex debuggingyourbraincover.latex _posts/2018-11-25-debugging-your-brain-1.md _posts/2018-12-16-debugging-your-brain-2.md _posts/2018-12-21-debugging-your-brain-3.md
