e20edbd91395e0a18503d90529b7963e44d33bd7
Casey Watts <casey.s.watts@gmail.com>
2020-07-10 14:41:12 -0400

    update build script for new file structure; run it once

--- book/build.sh
+++ book/build.sh
@@ -1,25 +1,11 @@
 #!/bin/sh -x
 
-sed "s/\/assets/assets/g" ../_posts/2018-11-25-debugging-your-brain-1.md > _chapters/chapter1.md && \
-sed "s/\/assets/assets/g" ../_posts/2018-12-16-debugging-your-brain-2.md > _chapters/chapter2.md && \
-sed "s/\/assets/assets/g" ../_posts/2019-12-28-debugging-your-brain-3.md > _chapters/chapter3.md && \
-sed "s/\/assets/assets/g" ../_posts/2019-12-28-debugging-your-brain-4.md > _chapters/chapter4.md && \
-sed "s/\/assets/assets/g" ../_posts/2020-03-19-debugging-your-brain-5.md > _chapters/chapter5.md
-
 pandoc -o debuggingyourbrain.epub \
 -f markdown_github+yaml_metadata_block \
 debuggingyourbraincoverepub.txt \
-_chapters/chapter1.md \
-_chapters/chapter2.md \
-_chapters/chapter3.md \
-_chapters/chapter4.md \
-_chapters/chapter5.md
+all-chapters.md
 
 pandoc -o debuggingyourbrain.pdf \
 -f markdown_github+yaml_metadata_block \
 -t latex debuggingyourbraincover.latex \
-_chapters/chapter1.md \
-_chapters/chapter2.md \
-_chapters/chapter3.md \
-_chapters/chapter4.md \
-_chapters/chapter5.md
+all-chapters.md
