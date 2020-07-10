fe34a114f68bbdc20cbc33cc09c22b06fec5ada0
Casey Watts <casey.s.watts@gmail.com>
2020-07-10 14:44:08 -0400

    export to a folder that doesn't get checked in

--- book/book_notes.md
+++
@@ -1,3 +0,0 @@
-In the following section you will learn many of the common pitfalls people encounter when processing these experiences, and strategies for avoiding or getting out of these.
-
-Being vulnerable is also a powerful way to deepen the relationship.
--- book/build.sh
+++ book/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -x
 
-pandoc -o debuggingyourbrain.epub \
+pandoc -o printed/debuggingyourbrain.epub \
 -f markdown_github+yaml_metadata_block \
 debuggingyourbraincoverepub.txt \
 all-chapters.md
 
-pandoc -o debuggingyourbrain.pdf \
+pandoc -o printed/debuggingyourbrain.pdf \
 -f markdown_github+yaml_metadata_block \
 -t latex debuggingyourbraincover.latex \
 all-chapters.md
