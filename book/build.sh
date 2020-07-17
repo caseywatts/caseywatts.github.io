d06edde71dbbb32dff2fb0a1544cd4f42eb0d7ec
Casey Watts <casey.s.watts@gmail.com>
2020-07-17 11:40:29 -0400

    move book to another repo

--- book/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-pandoc -s -f markdown --filter pandoc-citeproc --bibliography MyLibrary.json debuggingyourbrain.yaml all-chapters.md \
--o printed/debuggingyourbrain.epub --css=epub.css # --epub-embed-font='fonts/*.ttf'
-
-pandoc -s -f markdown --filter pandoc-citeproc --bibliography MyLibrary.json debuggingyourbrain.yaml all-chapters.md \
--t latex -o printed/debuggingyourbrain.pdf
--- book/buildwatch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -x
-
-while inotifywait -qqre modify all-chapters.md; do
- ./build.sh
-done
