dd4c3bd5b4eb56721b314f73e28b96688df4246b
Casey Watts <casey.s.watts@gmail.com>
2021-06-30 23:05:24 -0400

    purge isn't working right, it removes things like md:

--- postcss.config.js
+++ postcss.config.js
@@ -5,9 +5,6 @@ module.exports = {
     require('autoprefixer'),
     ...(process.env.JEKYLL_ENV == "production"
       ? [require('cssnano')({ preset: 'default' })]
-      : []),
-    require('@fullhuman/postcss-purgecss')({
-        content: ['./**/*.html', '*.html']
-    })
+      : [])
   ]
 }
